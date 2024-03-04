0bd8c2a04008d2461c3d70bb94238807ab926577
sanay21 <143115913+sanay21@users.noreply.github.com>
2024-03-04 22:42:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask, request, render_template , redirect , url_for
+from flask import Flask, render_template, request, redirect, url_for
+import re
 
 app = Flask(__name__)
 
-notes = []
-
-##################################
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        note = request.form.get('note')
-        if note:  
-            notes.append(note)  
-            return redirect(url_for('thankyou'))  
-    return render_template("home_page.html", notes=notes)
+        test_string = request.form['test_string']
+        regex_pattern = request.form['regex']
+        return redirect(url_for('results', test_string=test_string, regex_pattern=regex_pattern))
+    return render_template('index.html')
 
-@app.route('/thankyou')
-def thankyou():
-    return render_template("thankyou_page.html")
+@app.route('/results')
+def results():
+    test_string = request.args.get('test_string', '')
+    regex_pattern = request.args.get('regex_pattern', '')
+    matches = re.findall(regex_pattern, test_string)
+    return render_template('results.html', matches=matches, test_string=test_string, regex_pattern=regex_pattern)
 
-##################################
+@app.route('/validate_email', methods=['POST'])
+def validate_email():
+    email = request.form['email']
+    if re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+        return 'Valid Email'
+    else:
+        return 'Invalid Email'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
